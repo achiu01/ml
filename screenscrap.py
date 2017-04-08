2583faac13bbd07e70ba8d03259b843e5e66567b
ailin chiu <achiu01@gmail.com>
2017-04-08 15:32:11 -0700

    modified beautifulsoup call

--- screenscrap.py
+++ screenscrap.py
@@ -1,15 +1,21 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
 
-html = urlopen("http://www.bloomberg.com/quote/SPX:IND")
-soup = BeautifulSoup(html, 'html.parser')
+#read url and parse
+url = "http://www.bloomberg.com/quote/SPX:IND"
+page_soup = soup(uReq(url).read(), 'html.parser')
+#print(page_soup.h1)
 
 # Take out the <div> of name and get its value
-name_box = soup.find('h1', attrs={'class': 'name'})
-name = name_box.text.strip() # strip() is used to remove starting and trailing
+name_box = page_soup.find('h1', attrs={'class': 'name'})
+print(name_box)
+
+# strip() is used to remove starting and trailing
+name = name_box.text.strip()
+
 print(name)
 
 # get the index price
-price_box = soup.find('div', attrs={'class':'price'})
+price_box = page_soup.find('div', attrs={'class':'price'})
 price = price_box.text
 print(price)
